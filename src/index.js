23990e19d6221c41f5d56d36fa720544a6d4cf15
Arpan Vala <valaarpan05@gmail.com>
2025-01-29 21:19:16 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const server = http.createServer((req,res)=>{
+    if(req.url === '/' && req.method === 'GET'){
+        res.end('<H1>Welcome to our home page</h1>')
+    
+    }
+})
+server.listen(5000,()=>{
+    console.log('Server is listening on port 5000')
+})
