2044b9a199d664b5f5d7a353c25ccd8c87ba9069
Arpan Vala <valaarpan05@gmail.com>
2025-02-01 11:53:08 +0530

    user-module added

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,4 +1,14 @@
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+
+export const comparePasswords =(password,hash)=>{
+    return bcrypt.compare(password,hash)
+}
+
+export const hashPassword = password =>{
+    return bcrypt.hash(password)
+}
+
 export const createJWT = (user) =>{
     const token=jwt.sign({
         id:user.id,
@@ -7,10 +17,29 @@ export const createJWT = (user) =>{
     process.env.JWT_SECRET)
     return token
 }
-export const protect =(req,res)=>{
+export const protect =(req,res,next)=>{
     const bearer = req.headers.authorization
 
     if(!bearer){
+        res.status(401)
+        res.json({msg:"unauthorized access!"})
+        return
+    }
+    const [,token]=bearer.split(" ");
+    if(!token){
+         res.status(401)
+        res.json({msg:"unauthorized access!"})
+        return
+    }
+
+    try {
+        const user = jwt.varify(token,process.env.JWT_SECRET);
+        req.user = user;
+        console.log(user)
+        next()
+        return;
+    } catch (error) {
+        console.log(error)
         res.status(401)
         res.json({msg:"unauthorized access!"})
         return
